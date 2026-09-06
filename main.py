7335b0064edbd87fb70a3710e3741bf009dc2723
Azizu Ahmad <azizu.rozaki@gmail.com>
2026-09-06 20:37:16 +0700

    edit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 
-# shortcut biar bisa dijalankan lewat "python main.py" atau "uv run main.py"
 sys.path.insert(0, str(Path(__file__).parent / "src"))
 
 from pipeline import main
